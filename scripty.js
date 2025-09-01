0a167889f63b9e0395016297a53b675bc1703f87
alcides-rc <alcides.cuminesi@escola.pr.gov.br>
2025-09-01 16:02:55 -0300

    6

--- scripty.js
+++ scripty.js
@@ -1,10 +1,9 @@
-const avanca =document.querySelectorAll('.bnt-proximo');
+const avanca = document.querySelectorAll('.bnt-proximo');
 
-.avanca.forEach(button => {
-    button.addEventListener('click' function(){
-        const atual = document.querySelector('.ativo')
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
-
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
